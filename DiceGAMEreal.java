a8a85cdc4f777cacf3ccaafe503330b8e3bfbade
DiameterEffect <65675954+DiameterEffect@users.noreply.github.com>
2023-04-20 22:32:36 -0400

    Dice Game project
    Revised

--- DiceGAMEreal.java
+++ DiceGAMEreal.java
@@ -1,111 +1,209 @@
 import java.util.Scanner;
+
+import javax.sound.sampled.Port;
+
 import java.lang.Math;
-
-public class DiceGAMEreal {
+public class DiceGameThree {
     public static void main(String[] args) {
-
-
+        
         Scanner scannerObj = new Scanner(System.in);
-        String userNAME = "Admin";
-        String password = "Admin1";
-        int DICE1 = 6;
-        int DICE2 = 6;
-        //Takes user to login method
-        Login(userNAME,password);
-        ///Basic menu
-        System.out.println("Choose an option: ");
-        System.out.println("1. Dice Game");
-        System.out.println("2. Exit");
-        int optionUSER = scannerObj.nextInt();
-        ///User enters an options wether to leave or play the game
-             switch (optionUSER) {
+        
+        int choice = 0;
+
+        do
+        {
+            ////Copy down menu options
+            System.out.println("1. Rules");
+            System.out.println("2. Play against computers");
+            System.out.println("3. Play 2 player game");
+            System.out.println("4. See high score");
+            System.out.println("5. Quit");
+
+            choice = scannerObj.nextInt();
+            switch (choice)
+
+            {
+                
                 case 1:
-                    PlayGame(scannerObj,DICE1,DICE2);
+
+                    ///Rules(no method needed)
+                    rules();
                     break;
+                
                 case 2:
-                    System.out.println("Exiting program, good bye!");
+                    ///play againist computer
+                    AicomputerGame();
                     break;
-            
+                case 3:
+
+                    ///play 2 player game
+                    TwoPlayerGame();
+                    break;
+
+                case 4:
+
+                    ///See high score
+                    HighScore();
+                    break;
+                
+                case 5:
+
+                    ///quit
+                    break;
+
                 default:
-                    System.out.println("invaild option, please try again later, bye. ");
-                    
+                ///error log
+                    System.out.println("error, invaild number, try again.");
                     break;
             }
+        }while(choice!=5);
+        {
+            System.out.println("good bye");
+
+            scannerObj.close();
+
+        }
+
+
+
 
     }
 
-    public static void PlayGame(Scanner scannerObj,int DICE1,int DICE2) 
-
+    public static void rules() 
     {
-        System.out.println("Starting game");
+        System.out.println("Game rules:");
+        System.out.println("Player starts with 20 dollars");
+        System.out.println("If a player rolls a 7 or an 11 with two of the dice they");
+        System .out.println("win the game and they take the cash. If player rolls a 2-3-12 they lose.");
+        System .out.println("Player wins");
+    }
+    
+    public static void AicomputerGame() 
+    {
         
-        ///scoreboard, we are going to have it called every time the 
-        ///player goes, the scoreboard will be shown
-        ///Player starts with 40 dollars
 
-        int P1score = 0;
-        int P1money = 40;
-        int P2score = 0;
-        int P2money = 40;
-        int pot = 0;
+        double pot = 0;
+        double Player1BAL = 20;
+        double Player1BET = 0;
+        
+        double Player2BAL = 20;
+        double PLayer2BET = 0;
+        int DICE1 = 6;
+        int DICE2 = 6;
+        ///whatever dice they rolled
+        int Player1diceSUM = 0;
+        int Player2diceSUM = 0;
+
+        Scanner scannerObj = new Scanner(System.in);
+        ///let the player enter whatever username they want
+        System.out.println("Enter the username you want");
+        String Player1 = scannerObj.next();
+        System.out.println("Your username is: " + Player1);
+        ///Computer name
+        String userNAME2 = "CPU";
 
 
-        ///Enter logic
-        ///Calls in the method to generate it inside the array and therefore we get 2 values inside the array
-        ///Dice 1 and Dice 2 all randomly genearated
+
+        ///Prints both values for the dice
+
+        
+        ///player one goes first
+
+        System.out.println( Player1 + " 's turn!");
+        System.out.println(Player1 + " Max number you can bet is " + Player1BAL);
+        System.out.println(Player1 + " How much would you like to bet?");
+        double answ = scannerObj.nextDouble();
+
+        Player1BET = answ;
+
+        while(Player1BET < 0 || Player1BET > Player1BAL )
+        {
+            System.out.println("Error, you must select a number greater than 0 or");
+            System.out.println("less than or equal to your balance which is " + Player1BAL);
+            System.out.println(Player1 + " How much would you like to bet?");
+            double answ2 = scannerObj.nextDouble();
+            Player1BET = answ2;
+
+        }
+        
+        ///Pot gets updated
+        pot = pot + Player1BET;
+        ///print current pot?
+        ///Print current balance?
+
+        System.out.println(Player1 + "'s turn to roll dice!");
+
         int[] diceValues = diceGEN();
         DICE1 = diceValues[0];
         DICE2 = diceValues[1];
-        ///Needs to be stored in logEntries whenever we get the chance
+        ///delete after
         System.out.println("Values in diceValues: ");
+
         for(int i = 0;i <diceValues.length;i++)
         {
-            System.out.print(diceValues[i] + " ");
+            System.out.print(diceValues[i] + " '");
         }
-        System.out.println();
-        ///P1 turn
-        System.out.println("Player one turn");
-        System.out.println("Player one, how much would you like to bet? Current balance: " + P1money);
-        int answ = scannerObj.nextInt();
-        pot = pot + answ;
-        System.out.println("Current pot: " + pot);
-         P1money = P1money - answ;
-        System.out.println("Current bal: " + P1money);
-        System.out.println("Player one betted: " + answ);
-        System.out.println("Player one turn to roll dice");
-        P1score = DICE1+DICE2;
-        System.out.println("Player one rolled " + P1score);
-        ///P2 turn
-        System.out.println("Player two turn");
-        System.out.println("Player two, how much would you like to bet? Current balance: " + P2money);
-        int answ2 = scannerObj.nextInt();
-        pot = pot + answ2;
-        System.out.println("Current pot: " + pot);
-         P2money = P2money - answ2;
-        System.out.println("Current bal: " + P2money);
-        System.out.println("Player one betted: " + answ2);
-        System.out.println("Player one turn to roll dice");
-        P1score = DICE1+DICE2;
-        System.out.println("Player one rolled " + P2score);
-        
+        System.out.println("  ");
+        System.out.println("  ");
 
-        ///Game rules here in computer form
-        ///Basically player rolls 7 they win
-        ///Player rolls a 2-3-12 they lose
-        switch (P1score) {
-            case 7:
-                System.out.println("This player has 7!");
-                
-                break;
+        Player1diceSUM = DICE1+DICE2;
+
+        System.out.println(Player1 + " rolled " + Player1diceSUM);
+
+        System.out.println("CPUs turn");
+
+        int[] diceValues2 = diceGEN();
+        DICE1 = diceValues2[0];
+        DICE2 = diceValues2[1];
+
+        Player2diceSUM = DICE1+DICE2;
+        ///match the player
+        PLayer2BET = Player1BET;
+        pot = pot + PLayer2BET;
+
+        System.out.println("CPU rolled "+ Player2diceSUM);
+
+        System.out.println("Current pot is  "+ pot);
+        ///If its true or not
+        switch (Player1diceSUM) {
             case 2:
-
             case 3:
-
             case 12:
-                System.out.println("This player has rolled a 2,3, or 12!");
+                System.out.println("You rolled " + Player1diceSUM);
+                
+                System.out.println("You lost!");
+                System.out.print("CPU gets the pot");
+                pot = Player2BAL;
+                System.out.println(pot);
+                ///CPU gets money
                 break;
-        
-            default:
+            case 7:
+            case 11:
+                System.out.println("You win");
+                System.out.println(Player1 + " gets the pot!");
+                System.out.println(pot);
+                pot = Player1BAL;
+                break;
+        }
+        switch (Player2diceSUM) {
+            case 2:
+            case 3:
+            case 12:
+                System.out.println("You rolled " + Player2diceSUM);
+                
+                System.out.println("CPU lost!");
+                System.out.print( Player1 + " gets the pot");
+                System.out.println(pot);
+                pot = Player1BAL;
+                System.out.println("Your balance is now " + Player2BAL);
+                break;
+            case 7:
+            case 11:
+                System.out.println("You win");
+                System.out.println( " CPU gets the pot!");
+                System.out.println(pot);
+                pot = Player2BAL;
+                System.out.println("Your balance is now " + Player2BAL);
                 break;
         }
 
@@ -113,30 +211,49 @@ public class DiceGAMEreal {
 
 
         
-        
-
-        
 
 
 
 
-            ///if player wins, input if they wanna try again with the money they have, or exit the program
 
-            
 
-        ///Calling this generates the points
-        ///diceGEN(DICE1, DICE2);
-        
-        //diceGEN(DICE1, DICE2);
 
 
         
 
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+        System.out.println("Test");
         
     }
-    ///Have a seperate method generate the number of dice the player gets
-    ///Going to have two intergers called DICE1 and DICE2 passed
-    /// here and it will be returned in PlayGame method.
+
+    public static void TwoPlayerGame() 
+    {
+        System.out.println("Test23");
+        
+    }
+
+    public static void HighScore() 
+    {
+        ////For this we need to get Player 1 user name from
+        /// AIcomputerGame and then print the content into the file
+        ///ex John score is 20
+        System.out.println("High score system");    
+    }
+
     public static int[] diceGEN ()
     {
         int DICE1 = (int) (Math.random() * 6) + 1;
@@ -144,53 +261,11 @@ public class DiceGAMEreal {
         int[] diceValues = {DICE1, DICE2};
         return diceValues;
         ///This generates a number everytime its called
-
-
-        //System.out.println("Number for DICE1 is  " + DICE1);
-        //System.out.println("Number for DICE2 is  " + DICE2);
-        
         
     }
 
-    public static void Login(String userNAME, String password) 
-
-    {
-        Scanner scannerObj = new Scanner(System.in);
-        boolean flag = true;
-         //Processing if user enters the correct password or not
-        while (flag) {
-            System.out.println("Enter your username:");
-            String inputuser = scannerObj.next();
-            System.out.println("Enter your password:");
-            String inputuser2 = scannerObj.next();
-            if (inputuser.equals(userNAME) && inputuser2.equals(password)) {
-                ///line below we are going to log this into the file
-                System.out.println("Correct!!");
-                ///Once user gets it correct, loop turns off 
-                flag = false;
-
-                
-            }
-            else
-            {
-                System.out.println("Incorrect, try again!");
-            }
-            ///scannerObj.close();
-            
-        }
 
 
-        
-
-
-        
-    }
-
-    public static void LogEntries(String text) 
-    {
-        
-    }
-
+}
 
     
-}
